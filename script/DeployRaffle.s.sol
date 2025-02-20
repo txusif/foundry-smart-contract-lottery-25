d7bbd0622f7e924f1727ed3337c4dc9c13ed440c
txusif <txusif@gmail.com>
2025-02-20 19:57:20 +0530

    ⭐️ init commit

---
+++ script/DeployRaffle.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
+
+contract DeployRaffle is Script {}
