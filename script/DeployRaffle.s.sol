ac47707de825d3109f9c61d68997e84cd466b258
txusif <txusif@gmail.com>
2025-02-26 23:23:16 +0530

    deploy a mock chainlink VRF

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -2,7 +2,12 @@
 
 pragma solidity ^0.8.19;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 import {Raffle} from "../src/Raffle.sol";
 
-contract DeployRaffle is Script {}
+contract DeployRaffle is Script {
+    function deployContract() public returns (Raffle, HelperConfig) {}
+
+    function run() public {}
+}
